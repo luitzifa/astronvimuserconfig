809c51b7194c3e7aae323ddce53f7622a1efb750
Heinz-Daniel Krämer <dakr@solute.de>
2023-08-25 10:31:11 +0200

    Use solute-pyformat in null-ls not feature-complete

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -3,6 +3,23 @@ return {
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
+    local h = require("null-ls.helpers")
+    local u = require("null-ls.utils")
+    local solutepyformat_formatter = {
+        name = "solutepyformat",
+        method = null_ls.methods.FORMATTING,
+        filetypes = { "python" },
+        generator = null_ls.formatter({
+            command = "/home/dakr/.local/solute/tools/solute-pyformat",
+            args = { },
+            to_stdin = true,
+            cwd = h.cache.by_bufnr(function(params)
+              return u.root_pattern(
+                "pyproject.toml"
+              )(params.bufname)
+            end),
+        }),
+    }
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -14,8 +31,9 @@ return {
       null_ls.builtins.diagnostics.puppet_lint,
       null_ls.builtins.formatting.puppet_lint,
       null_ls.builtins.diagnostics.shellcheck,
-      null_ls.builtins.formatting.black,
+      -- null_ls.builtins.formatting.black,
       null_ls.builtins.diagnostics.pylint.with({ prefer_local = true }),
+      null_ls.register(solutepyformat_formatter)
     }
     return config -- return final config table
   end,
